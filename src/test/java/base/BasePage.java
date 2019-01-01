ba216c6f21422357d8f7fdd4c7d27bbe9024ae41
Igor Timofeev <tiy2000@mail.ru>
2019-01-01 11:33:53 +0700

    Add waiting for elements in BasePage class

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,11 +1,17 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
     private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
+    WebDriverWait wait;
+    protected int defaultTimeOutInSeconds = 15;
 
 
 
@@ -26,6 +32,22 @@ public class BasePage {
     static public boolean isDriverCreated() {
         return getDriver() != null;
     }
+
+
+
+    // ===== Waiting for elements
+
+    public WebElement waitElement(By by) throws TimeoutException {
+        WebElement element = wait.until(driver -> driver.findElement(by));
+        return element;
+    }
+
+    public void waitElementDisplayed(By by) throws TimeoutException {
+        wait.until(driver -> driver.findElement(by).isDisplayed());
+    }
+
+
+
 
 
 }
