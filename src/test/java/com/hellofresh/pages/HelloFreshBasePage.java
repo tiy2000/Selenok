d9a5bc902e6ac5def2ed9696d91b663a60968b84
Igor Timofeev <tiy2000@mail.ru>
2019-01-02 14:53:36 +0700

    Add implicitlyWait 1 second in HelloFreshBasePage

--- src/test/java/com/hellofresh/pages/HelloFreshBasePage.java
+++ src/test/java/com/hellofresh/pages/HelloFreshBasePage.java
@@ -2,6 +2,8 @@ package com.hellofresh.pages;
 
 import base.BasePage;
 
+import java.util.concurrent.TimeUnit;
+
 public class HelloFreshBasePage extends BasePage {
 
     static {
@@ -10,5 +12,7 @@ public class HelloFreshBasePage extends BasePage {
 
     public HelloFreshBasePage() {
         setDefaultTimeOutInSeconds(5);
+        getDriver().manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        getDriver().manage().timeouts().pageLoadTimeout(1, TimeUnit.SECONDS);
     }
 }
