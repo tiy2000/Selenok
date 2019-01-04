26a99bf11450c76e0174dabcf8acc93c4bb69734
Igor Timofeev <tiy2000@mail.ru>
2019-01-04 09:33:01 +0700

    Add @Step annotations to HelloFreshBasePage

--- src/test/java/com/hellofresh/pages/HelloFreshBasePage.java
+++ src/test/java/com/hellofresh/pages/HelloFreshBasePage.java
@@ -1,6 +1,8 @@
 package com.hellofresh.pages;
 
 import base.BasePage;
+import base.InvalidPageStateException;
+import io.qameta.allure.Step;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,5 +16,17 @@ public class HelloFreshBasePage<T extends BasePage> extends BasePage<T> {
         setDefaultTimeOutInSeconds(5);
         getDriver().manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 //        getDriver().manage().timeouts().pageLoadTimeout(1, TimeUnit.SECONDS);
+    }
+
+    @Step
+    @Override
+    public T validateIsRightPage() throws InvalidPageStateException {
+        return super.validateIsRightPage();
+    }
+
+    @Step
+    @Override
+    public T assertRightPage() throws AssertionError {
+        return super.assertRightPage();
     }
 }
